fefd443b67be3b4fc1fe8c3c87e7d1faf2419a75
88botong <34149882+88botong@users.noreply.github.com>
2024-10-09 23:49:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from midiutil.MidiFile import MIDIFile
 from osupyparser import OsuFile, Circle
 
-# Load the OSU beatmap file
-osu_beatmap = OsuFile("beatmap.osu").parse_file()
+# Load the osu! beatmap file
+directory = input("Directory: ")
+osu_beatmap = OsuFile(directory).parse_file()
 # Create a new MIDI file
 midi_file = MIDIFile(1)
 
@@ -22,7 +23,7 @@ for hit_object in osu_beatmap.hit_objects:
 
     if hit_object.__class__ == Circle:  # Hit circle
         note_time = hit_object.start_time / 60000 * tempo  # Convert note time in ms to beat number
-        note_pitch = 60  # Set the note pitch to middle C
+        note_pitch = 60  # Set the note pitch to C3
         note_duration = 1  # Set the note duration to 1 beat
         note_velocity = 127
         midi_file.addNote(track, channel, note_pitch, note_time, note_duration, note_velocity)
